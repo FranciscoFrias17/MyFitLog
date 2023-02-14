318f73a819bf08a5153961851a7e94229d3e5e6c
Francisco Frias <Francisco.frias17@gmail.com>
2023-02-13 23:52:09 -0500

    Added TS and edited settings
    added TS and edited settings

--- backend/controller/exerciseController.js
+++ backend/controller/exerciseController.js
@@ -2,7 +2,7 @@
 // @route: GET /api/exercises
 
 const getExercise = (req, res) => {
-    res.status(200).json({message: 'Get exercise'})
+    res.status(200).json({ message: 'Get exercise' })
 }
 
 module.exports = {
--- backend/routes/exerciseRoutes.js
+++ backend/routes/exerciseRoutes.js
@@ -5,15 +5,15 @@ const { getExercise } = require('../controller/exerciseController')
 router.get('/', getExercise)
 
 router.post('/', (req, res) => {
-    res.status(200).json({message: 'Set exercise'})
+    res.status(200).json({ message: 'Set exercise' })
 })
 
 router.put('/:id', (req, res) => {
-    res.status(200).json({message: `Update exercise ${req.params.id}`})
+    res.status(200).json({ message: `Update exercise ${req.params.id}` })
 })
 
 router.delete('/:id', (req, res) => {
-    res.status(200).json({message: `Delete exercise ${req.params.id}`})
+    res.status(200).json({ message: `Delete exercise ${req.params.id}` })
 })
 
 module.exports = router
