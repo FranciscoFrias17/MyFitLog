8e13db4f2d335855c9b665059c43441a79af0e73
Francisco Frias <Francisco.frias17@gmail.com>
2023-03-07 00:51:53 -0500

    Create authMiddleware.ts

---
+++ backend/middleware/authMiddleware.ts
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken'
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel'
+import { Request, Response, NextFunction } from 'express'
+
+export const protect = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    let token
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            // Get token from header
+            token = req.headers.authorization.split(' ')[1]
+
+            // Verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET as string)
+
+            // Get user from token
+            req.user = await User.findById((decoded as any).id).select('-password')
+
+            next()
+        } catch (error) {
+            console.error(error)
+            res.status(401)
+            throw new Error('Not authorized, token failed')
+        }
+    }
+
+    if (!token) {
+        res.status(401)
+        throw new Error('Not authorized, no token')
+    }
+})
